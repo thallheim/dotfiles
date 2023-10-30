8c73a10cff199922768c00ff61e1ba9057efafbe
thallheim <91142701+thallheim@users.noreply.github.com>
2023-10-30 21:32:43 +0100

    Cleanup output functions

--- repo-update/functions.sh
+++ repo-update/functions.sh
@@ -25,6 +25,7 @@ reset=$(tput sgr0)
 # MSG STRINGS
 red_cross="$red""\xE2\x9D\x8C""$reset"
 green_checkmark="$green""\xE2\x9C\x93""$reset"
+info_arrow="$cyan""\xE2\x87\xA8""$reset"
 error_label="[$red""ERROR""$reset""]"
 info_label="[$cyan""INFO""$reset""]"
 warn_label="[$yellow""WARN""$reset""]"
@@ -89,7 +90,7 @@ function read_inc_list() {
 
 function verify_src_paths() {
     local result=""
-    printf "[$cyan""INFO""$reset""] Verifying sources\n"
+    printf "$info_arrow"" $info_label"" Verifying sources\n"
 
     for path in "${input_paths[@]}"; do
 	if [ -e "$path" ]; then
