3e56ffa7ff99a87e1b3a29cacf358b70e1486534
thallheim <robin@snowy>
2023-10-30 09:20:16 +0100

    Added some flair

--- repo-update/functions.sh
+++ repo-update/functions.sh
@@ -13,12 +13,23 @@ inc_list="./inclusions.dat"
 target_root="${HOME}/dotfiles/"
 emacs_target_root="${HOME}/dotfiles/emacs/"
 
+# COLOURS
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+cyan=$(tput setaf 6)
+reset=$(tput sgr0
 
 # MSG STRINGS
-info_copied_msg="[INFO] Source is newer. Target updated."
-error_target_newer="[ERROR] Target file is newer: Exiting..."
+info_copied_msg='[\[$blue\]INFO\[$reset\]] Source is newer. Target updated.'
+error_target_newer="$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" Target file is newer: Exiting..."
 warn_src_trgt_eq="[INFO] Nothing to do: Source and target have identical modification times."
 info_diff_eq_msg="[INFO] Nothing to do: Files up to date."
+
+)
+
 
 # CHECKS
 inc_list_ok=false
@@ -46,7 +57,7 @@ function inclusion_list_exists() {
     if [[ -e "$inc_list" ]]; then
 	return 0
     else
-	printf "[ERROR] Could not read inclusion list:\n%s\n " "$inc_list"
+	printf "$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" Could not read inclusion list:\n%s\n " "$inc_list"
 	return 1
     fi
 }
@@ -56,14 +67,14 @@ function check_target_paths() {
     local path="$1"
     # exists and is a dir
     if [ -e "$path" ] && [ -d "$path" ]; then
-	printf "[ERROR] Path exists, but is a directory\n"
+	printf "$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" Path exists, but is a directory\n"
 	exit 1
     # exists and isn't a dir
     elif [ -e "$path" ] && [ ! -d "$path" ]; then
 	return 0
     # doesn't exist
     else
-	printf "[ERROR] The path doesn't exist.\n"
+	printf "$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" The path doesn't exist.\n"
 	exit 1
     fi
 }
@@ -74,22 +85,22 @@ function read_inc_list() {
 	    input_paths+=("$(eval echo "$line")")
 	done < "$inc_list"
     else
-	printf "[ERROR] Could not find inclusion list:\n'%s'\n" "$inc_list"
+	printf "$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" Could not find inclusion list:\n'%s'\n" "$inc_list"
 	exit 1
     fi
 }
 
 function verify_src_paths() {
     local result=""
-    printf "[INFO] Verifying sources\n"
+    printf "[$cyan""INFO""$reset""] Verifying sources\n"
+
     for path in "${input_paths[@]}"; do
 	if [ -e "$path" ]; then
 	    result=("$(strip_home_slug "$path")")
-	    printf "[INFO] OK: %s\n" "${result[@]}"
+	    printf "$green""\xE2\x9C\x93""$reset"" [$cyan""INFO""$reset""]"" %s\n" "${result[@]}"
 	else
-	    printf "[ERROR] Requested file not found:\n'%s'\n" "$path"
-	    
+	    printf "$red""\xE2\x9D\x8C""$reset"" [$red""ERROR""$reset""]"" File not found: '%s'\n" "$path"
 	fi
     done
-#    printf "[INFO] OK: %s\n" "${result[@]}"
+
 }
