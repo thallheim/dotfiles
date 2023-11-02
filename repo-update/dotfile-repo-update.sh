7316744c9462e90b55300b7142d26b5f5ee4521a
thallheim <91142701+thallheim@users.noreply.github.com>
2023-11-02 06:57:22 +0100

    Add CWD guard, factor out error handling

--- repo-update/dotfile-repo-update.sh
+++ repo-update/dotfile-repo-update.sh
@@ -1,55 +1,34 @@
 #!/bin/bash
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #
 #       TODO: Proper description
 #       For keeping my dotfile repo mostly in order (and not submoduled).
 #       Also actually updated. 
+#
+#       (!) NOTE:
+#       The inclusion file needs to end with a blank line, or the last entry
+#       will not be read. TODO: Work out why.
 #       
 #      
 #       TODO: Logging, maybe?
 #      
-#       1. Reimplement path checks
-#       2. Reimplement age checks
-#       3. Reimplement copy ops
 #      
 #      
 #       thall.
 #       <thall@thallheim.com>
 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+# Get, and cd to, script's dir in case it's called from elsewhere
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$script_dir"
+
 
 source "./functions.sh"
-error_detected=0
+# TODO: Remove: Debug only
+#printf "ARGV[1] PASSED = \t%s\n\n" "$arg1"
 
-inclusion_list_exists || error_detected=1
-read_inc_list || error_detected=1
-verify_src_paths || error_detected=1
-
-if [ "$error_detected" -eq 0 ]; then
-    echo "It wörks!"
-else
-    echo "It's bwoken."
-fi
-
-
-# CHECK: diff
-#if diff "$emacs_target_filepath" "$emacs_file_src" &>/dev/null; then
-#    printf '%s\n' "${diff_eq_msg}"
-#    exit 0
-#else
-#    ## CHECK: stat
-#    src_timestamp=$(stat -c %Y "$emacs_file_src")
-#    target_timestamp=$(stat -c %Y "$emacs_file_src_target")
-#    if [[ "$src_timestamp" -gt "$target_timestamp" ]]; then
-#	cp "$emacs_file_src" "$emacs_target_filepath"
-#	printf '%s\n' "$copied_msg"
-#    elif [[ "$src_timestamp" -lt "$target_timestamp" ]]; then
-#	printf '%s\n' "$error_target_newer"
-#	exit 1
-## Should be unreachable, yes?
-##    else
-##	printf '%s\n' "$warn_src_trgt_eq"
-#    fi
-#fi
-
+read_inc_list
+verify_src_paths
