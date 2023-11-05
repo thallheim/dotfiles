76ee1f9aaa40d7bcceb6bfc07a9a3e08a6ec3a07
thallheim <91142701+thallheim@users.noreply.github.com>
2023-11-05 01:19:40 +0100

    Clean up + move all ops handling into arg parsing

--- repo-update/dotfile-repo-update.sh
+++ repo-update/dotfile-repo-update.sh
@@ -22,12 +22,10 @@ true
 source "./functions.sh"
 source "./parse_args.sh"
 
-# Get, and cd to, script's dir in case it's called from somewhere else
 cd_own_dir
-
 parse_args "$@"
-read_inc_list
-verify_src_paths
+
+
 # TODO: Check and/or create dst dirs
 
 # TODO: Copy the lot
