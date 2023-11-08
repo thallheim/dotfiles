39cb3787b66ea7857889b1070b55431668318efa
thallheim <thall@thallheim.com>
2023-11-08 03:29:44 +0100

    Add colours; sort out formatting

--- repo-update/colours.sh
+++ repo-update/colours.sh
@@ -1,14 +1,16 @@
-reset=$(tput sgr0)
+#!/bin/bash
+# shellcheck disable=2046,2154,2155
 
-black=$(tput setaf 0)
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
-cyan=$(tput setaf 6)
+export reset=$(tput sgr0)
+export black=$(tput setaf 0)
 
-bold=$(tput bold)
-end_bold=$(tput sgr0)
+export bold=$(tput bold); export end_bold=$(tput sgr0)
+export italic=$(tput sitm); export end_italic=$(tput ritm)
 
-italic=$(tput sitm)
-end_italic=$(tput ritm)
+export darkblue=$(tput setaf 4); export blue=$(tput setaf 12)
+export darkgrey=$(tput setaf 8); export grey=$(tput setaf 7)
+export darkred=$(tput setaf 1); export red=$(tput setaf 9)
+export darkgreen=$(tput setaf 2); export green=$(tput setaf 10)
+export darkyellow=$(tput setaf 3); export yellow=$(tput setaf 11)
+export darkmagenta=$(tput setaf 5); export magenta=$(tput setaf 13)
+export darkcyan=$(tput setaf 6); export cyan=$(tput setaf 14)
