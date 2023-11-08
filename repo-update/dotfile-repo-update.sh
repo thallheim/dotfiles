78b57c7ab615c377531cca79caefb943569ab194
thallheim <thall@thallheim.com>
2023-11-08 07:28:13 +0100

    Add up-to-date file summaries to runner .sh

--- repo-update/dotfile-repo-update.sh
+++ repo-update/dotfile-repo-update.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 # shellcheck disable=1091,2154,2059 # 'source-path=SCRIPTDIR' isn't working
-true
+
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #
-#       TODO: Proper description
-#       For keeping my dotfile repo mostly in order (and not submoduled).
-#       Also actually updated. 
+# (!) The inclusion file needs to end with a blank line, or the last
+# entry isn't read. TODO: Work out why
 #
-#       (!) NOTE:
-#       The inclusion file needs to end with a blank line, or the last entry
-#       will not be read. TODO: Work out why.
+#
+# FILE SUMMARIES:
 #       
-#      
-#      
-#       thall.						
+# - INCLUSIONS.DAT
+#	Newline-delimited list of files to be pulled into the dst dir
 #
+# - INFO_ERRORS.SH
+#	Strings, labels, functions for info and error handling
+#      
+# - FUNCTIONS.SH
+#	File and directory handlers, as well as whatever didn't quite
+#	merit factoring out.
+#
+# - HELPERS.SH
+#	Helper functions
+#
+# - PARSE_ARGS.SH
+#	Contains the arg parser and related flags
 #
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
