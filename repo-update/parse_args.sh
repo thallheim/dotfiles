8ad99ee804930edb3d50be3aaa458d370c4fde04
thallheim <thall@thallheim.com>
2023-11-11 07:17:23 +0100

    Rework how -v checking works. Verbosity flag now order-agnostic.

--- repo-update/parse_args.sh
+++ repo-update/parse_args.sh
@@ -5,32 +5,41 @@
 VALID_OPTS=""
 VALID_OPTS=$( getopt -o :vuthsV --long verbose,update,test,help,status,version -- "$@" )
 
+
+function request_verbose() {
+    if [[ "$*" =~ "v" && $FLAG_VERBOSE = false ]]; then
+        FLAG_VERBOSE=true
+	printf "${MAGENTA}Verbose mode:${RESET}\t%s\n \
+${MAGENTA}ARGS:${RESET}\t\t%s\n" \
+"${GREEN}${FLAG_VERBOSE}${RESET}" "${YELLOW}$*${RESET}"
+    else
+        return
+    fi
+}
+
 function parse_args() {
 eval set -- "$VALID_OPTS"
-
-    [ $? -eq 0 ] || {
-        exit_fatal "No recognised option provided"
-    }
 
 while true; do
      case "$1" in
 	 -v | --verbose)
-	     FLAG_VERBOSE=true 
-	     printf "${MAGENTA}Verbose mode:${RESET}\t%s\n \
-${MAGENTA}ARGS:${RESET}\t\t%s\n" \
-"${GREEN}${FLAG_VERBOSE}${RESET}" "${YELLOW}$*${RESET}"
+	     request_verbose
 	     shift;;
 	    
-	-u | --update)
+	 -u | --update)
+	     request_verbose "$@"
+	     get_src_paths
+	     verify_src_readable
+	     get_dst_dirs
+	     mk_dst_dirs
+	     copy_all
+	     exit_done;;
+
+	-t | --test)
+	    request_verbose "$@"
 	    get_src_paths
 	    verify_src_readable
 	    get_dst_dirs
-	    mk_dst_dirs
-	    copy_all
-	    exit_done;;
-
-	-t | --test)
-	    echo "test :)"
 	    exit_done;;
 
 	-h | --help)
@@ -44,8 +53,9 @@ ${MAGENTA}ARGS:${RESET}\t\t%s\n" \
 	-V | --version)
 	    printf "dotupdate %s\n\nLicense: MIT\nWritten by thallheim.\n" "${VERSION}"
 	    exit 0;;
-
+	
 	--)
+	    error "Unknown option"
 	    show_help
 	    shift
 	    break;;
